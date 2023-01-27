9bf98ed23e0ff3adbc2879ef576fd7027d8a57a2
ArtNeonatolog <marcus201@mail.ru>
2023-01-27 23:01:17 +0500

    Не совсем понял как через assertThrows реализовать тест на проверку "@" и . в e-mail. Верна ли у меня реализация и обязательно ли всегда через assertThrows надо? Еще вопрос про тест без параметров, там же если null задать логин и пароль тест не пройдет, тк не пройдет проверку на эквивалентность. Я просто пустым оставил конструктор, так можно?

--- src/test/java/me/artsafuanov/homeworkproject/UserTest.java
+++ src/test/java/me/artsafuanov/homeworkproject/UserTest.java
@@ -1,14 +1,13 @@
 package me.artsafuanov.homeworkproject;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
     @Test
     public void testUserConstructorWithParameters() {
-        final String login = "login";
-        final String email = "login@mail.com";
+        String login = "login";
+        String email = "login@mail.com";
 
         User user = new User(login, email);
 
@@ -25,17 +24,28 @@ public class UserTest {
 
         assertNotNull(user);
     }
-
     @Test
     public void testUserLoginAndEmailEquals() {
-        final String login = "login";
+        final String login = "login@mail.com";
         final String email = "login@mail.com";
+
+        RuntimeException thrown = assertThrows(
+                RuntimeException.class,
+                () -> new User(login, email));
+        assertTrue(thrown.getMessage().contentEquals("Логин и e-mail не должны совпадать!"));
+    }
+
+    @Test
+    public void testEmailContains() {
+
+        String login = "login";
+        String email = "login@mail.com";
 
         User user = new User(login, email);
 
-        String userLogin = user.getLogin();
         String userEmail = user.getEmail();
-
-        assertNotEquals(userLogin, userEmail);
+        if (userEmail.contains("@") && userEmail.contains(".")){
+            assertSame(userEmail, user.getEmail());
+        }
     }
 }
